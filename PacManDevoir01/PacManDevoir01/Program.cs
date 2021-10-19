bde9431a943bc048412e1b2fc861c039bf4cedfc
MathieuLabre <81266491+MathieuLabre@users.noreply.github.com>
2021-10-19 17:11:22 -0400

    1

---
+++ PacManDevoir01/PacManDevoir01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PacManDevoir01
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}

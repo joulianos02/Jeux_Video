7bea1f8db79972e87558a840d5c0823b1f74102f
MathieuLabre <81266491+MathieuLabre@users.noreply.github.com>
2021-10-20 11:45:31 -0400

    Update Cherry

--- PacManDevoir01/PacManDevoir01/Game1.cs
+++ PacManDevoir01/PacManDevoir01/Game1.cs
@@ -14,25 +14,20 @@ namespace PacManDevoir01
 
         private Texture2D PacManRight1;
         private Texture2D PacManRight2;
-
         private Texture2D PacManLeft1;
         private Texture2D PacManLeft2;
-
         private Texture2D PacmanUP;
-
         private Texture2D PacMandown;
 
         private Texture2D Ghost1;
-   
-
-
+        private Texture2D Cherry;
         private Texture2D Pacman;
       
 
         Vector2 positionPacman;
         Vector2 positionGhost;
-
-       
+        Vector2 positionCherry; 
+    
 
         public Game1()
         {
@@ -46,23 +41,19 @@ namespace PacManDevoir01
         {
             // TODO: Add your initialization logic here
 
-            //la Position initiale du PacMan
-
+            //la Position de PacMan
             positionPacman = new Vector2(100.0f, 100.0f);
             positionPacman.X = 325.0f;
             positionPacman.Y = 325.0f;
-            //
+            //Possitionment de Ghost sur le background
             positionGhost = new Vector2(100.0f, 100.0f);
             positionGhost.X = 40.0f;
             positionGhost.Y = 40.0f;
-
-
-
-
-
             //
-
-
+            positionCherry = new Vector2(100.0f, 100.0f);
+            positionCherry.X = 400.0f;
+            positionCherry.Y = 40.0f;
+          
             base.Initialize();
         }
 
@@ -84,9 +75,7 @@ namespace PacManDevoir01
             PacMandown = Content.Load<Texture2D>("Pac-2 Down");
             //
             Ghost1 = Content.Load<Texture2D>("ghost-r1");
-
-
-
+            Cherry = Content.Load<Texture2D>("cherry");
 
 
             Pacman = _PacmanAvant;
@@ -156,6 +145,8 @@ namespace PacManDevoir01
 
             _spriteBatch.Draw(Pacman, positionPacman, Color.White);
             _spriteBatch.Draw(Ghost1, positionGhost, Color.White);
+            _spriteBatch.Draw(Cherry, positionCherry, Color.White);
+
 
 
 
